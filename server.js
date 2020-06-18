c13fb6d945268ebbc2c0e9cff71d4f3781663466
Nanfe05 <hernan05felipe@gmail.com>
2020-06-18 18:53:06 -0500

    proxy

--- server.js
+++ server.js
@@ -2,6 +2,11 @@ const express = require('express');
 const app = express();
 const port = 80;
 
+const {createProxyMiddleware} = require('http-proxy-middleware');
+
+
+app.use('/',createProxyMiddleware({target:'http://127.0.0.1:5000/',changeOrigin:true}));
+
 app.use(express.static('build'));
 
 app.listen(port,()=>{
