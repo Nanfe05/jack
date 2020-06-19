be09b6ff167250a7c68400b686bce91b6f609e26
Nanfe05 <hernan05felipe@gmail.com>
2020-06-18 19:54:19 -0500

    debug

--- server.js
+++ server.js
@@ -1,13 +1,18 @@
 const express = require('express');
 const app = express();
 const port = 80;
+const path = require('path');
 
 const {createProxyMiddleware} = require('http-proxy-middleware');
 
 
 app.use('/jackmarketing',createProxyMiddleware({target:'http://127.0.0.1:5000/',changeOrigin:true}));
 
-app.use('*',express.static('build'));
+app.use(express.static(path.join(__dirname,'build')));
+
+app.get('*',(req,res)=>{
+    res.sendFile(path.join(__dirname+'/build/index.html'));
+});
 
 app.listen(port,()=>{
     console.log('App Started');
