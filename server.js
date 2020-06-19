9f91f82c1cdff8e247ce1d76a92f95c3bd78a1ff
Nanfe05 <hernan05felipe@gmail.com>
2020-06-18 19:48:02 -0500

    debug

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const {createProxyMiddleware} = require('http-proxy-middleware');
 
 app.use('/jackmarketing',createProxyMiddleware({target:'http://127.0.0.1:5000/',changeOrigin:true}));
 
-app.use(express.static('build'));
+app.use('*',express.static('build'));
 
 app.listen(port,()=>{
     console.log('App Started');
